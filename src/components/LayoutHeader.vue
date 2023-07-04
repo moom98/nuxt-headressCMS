ed8abb773b4b6e76825e58a5a33eafa738ef6f8c
ymkmy98 <ymkmy98@gmail.com>
2023-07-04 14:12:03 +0900

    ナビゲーションにアニメ追加

--- src/components/LayoutHeader.vue
+++ src/components/LayoutHeader.vue
@@ -1,23 +1,25 @@
 <template>
-  <header class="header">
+  <header id="header" class="header">
     <nuxt-link to="/" href="" class="header__link">
       <h1 class="header__title">Maigo</h1>
     </nuxt-link>
-    <button class="header__button">
+    <button id="navBtn" class="header__button" @click="navToggle">
       <span class="header__button-top"></span>
       <span class="header__button-middle"></span>
       <span class="header__button-bottom"></span>
     </button>
 
-    <nav class="gnav">
+    <nav id="gnav" class="gnav">
       <ul class="gnav__list">
         <li v-for="(item, index) in links" :key="index" class="gnav__item">
-          <nuxt-link :to="item.path" class="gnav__link"
+          <a :href="item.path" class="gnav__link" @click="handleLinkClick(item)"
             >{{ item.label }}
-          </nuxt-link>
+          </a>
         </li>
       </ul>
     </nav>
+
+    <div id="mask" class="mask" @click="navClose"></div>
   </header>
 </template>
 
@@ -45,6 +47,24 @@ export default {
       ],
     }
   },
+
+  methods: {
+    navToggle() {
+      const header = document.querySelector('#header')
+      header.classList.toggle('is-active')
+    },
+
+    navClose() {
+      const header = document.querySelector('#header')
+      header.classList.remove('is-active')
+    },
+
+    handleLinkClick(item) {
+      this.navClose()
+      // メソッドを実行して遷移先のURLにリダイレクト
+      this.$router.push(item.path)
+    },
+  },
 }
 </script>
 
@@ -55,7 +75,7 @@ export default {
   align-items: center;
   justify-content: space-between;
   width: 100%;
-  padding: 0 20px 0 40px;
+  padding: 20px 40px;
 
   &__link {
     color: $mainColor;
@@ -69,13 +89,29 @@ export default {
 
   &__button {
     display: flex;
+    position: relative;
+    z-index: 20;
     flex-direction: column;
     align-items: center;
     justify-content: center;
-    gap: 10px;
+    gap: 8px;
     background-color: transparent;
     border: none;
     cursor: pointer;
+
+    .header.is-active & {
+      .header__button-top {
+        transform: translateY(10px) rotate(315deg);
+        background-color: #fff;
+      }
+      .header__button-middle {
+        opacity: 0;
+      }
+      .header__button-bottom {
+        transform: translateY(-10px) rotate(-315deg);
+        background-color: #fff;
+      }
+    }
   }
 
   &__button-top,
@@ -89,18 +125,40 @@ export default {
   }
 }
 
-.gnav {
+.mask {
   display: none;
-
   position: fixed;
   top: 0;
   left: 0;
+  width: 100%;
+  height: 100%;
+  background: #000;
+  opacity: 0.8;
   z-index: 10;
+  cursor: pointer;
+
+  .header.is-active & {
+    display: block;
+  }
+}
+
+.gnav {
+  position: fixed;
+  top: 0;
+  left: -300px;
+  z-index: 20;
+  opacity: 0;
   width: 300px;
   height: 100vh;
   padding: 60px 25px;
   color: #fff;
   background-color: #121212;
+  transition: all 0.5s ease;
+
+  .header.is-active & {
+    left: 0;
+    opacity: 1;
+  }
 
   &__link {
     display: block;
